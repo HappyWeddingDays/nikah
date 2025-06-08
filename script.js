e7a8b1aad57168bda3d063ec31afb59ea7c961c7
Sugeng Saputra <141040198+sugengsaputra99@users.noreply.github.com>
2025-06-08 19:33:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 AOS.init();
+
+
+//snow 
+  const snowContainer = document.getElementById("snow-block");
+
+  function createSnowflake() {
+    const snowflake = document.createElement("div");
+    snowflake.classList.add("snowflake");
+    snowflake.textContent = "❄";
+
+    // Posisi acak
+    snowflake.style.left = Math.random() * 100 + "%";
+    snowflake.style.animationDuration = 2 + Math.random() * 3 + "s";
+    snowflake.style.fontSize = 10 + Math.random() * 20 + "px";
+
+    snowContainer.appendChild(snowflake);
+
+    // Hapus setelah selesai jatuh
+    setTimeout(() => {
+      snowflake.remove();
+    }, 5000);
+  }
+
+  setInterval(createSnowflake, 300);
