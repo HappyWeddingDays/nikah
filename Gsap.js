b416eac6898b7a7ec20b22278d68ac6c75e855f7
Sugeng Saputra <141040198+HappyWeddingDays@users.noreply.github.com>
2025-06-10 20:06:24 +0700

    Update Gsap.js

--- Gsap.js
+++ Gsap.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
+/* gsap.registerPlugin(ScrollTrigger);
 
 // Fungsi pecah teks menjadi span huruf-huruf
 function splitTextToSpans(element) {
@@ -12,7 +12,7 @@ function splitTextToSpans(element) {
     span.style.opacity = 1;
     element.appendChild(span);
   });
-}
+} 
 
 // Ambil semua elemen dengan class explode
 const targets = document.querySelectorAll('.explode');
@@ -59,4 +59,90 @@ targets.forEach(el => {
       });
     }
   });
+}); */
+
+// JavaScript
+gsap.registerPlugin(ScrollTrigger);
+
+const element = document.querySelector('.typing-explode');
+const text = "Ini adalah efek ketikan seperti keyboard dan meledak.";
+const duration = 4000;
+const interval = duration / text.length;
+
+// 1. Efek Ketik
+ScrollTrigger.create({
+  trigger: element,
+  start: "top 50%",
+  once: true,
+  onEnter: () => {
+    let index = 0;
+    element.textContent = '';
+    const typing = setInterval(() => {
+      if (index < text.length) {
+        element.textContent += text[index];
+        index++;
+      } else {
+        clearInterval(typing);
+        setTimeout(() => {
+          explodeInit(); // Jalankan efek meledak setelah ketik selesai
+        }, 200); // jeda sejenak setelah selesai ketik
+      }
+    }, interval);
+  }
 });
+
+// 2. Fungsi pecah teks jadi <span> huruf-huruf
+function splitTextToSpans(element) {
+  const rawText = element.textContent;
+  element.innerHTML = '';
+  rawText.split('').forEach(char => {
+    const span = document.createElement('span');
+    span.textContent = char === ' ' ? '\u00A0' : char;
+    span.style.display = 'inline-block';
+    span.style.opacity = 1;
+    element.appendChild(span);
+  });
+}
+
+// 3. Fungsi efek meledak
+function explodeInit() {
+  splitTextToSpans(element);
+  const spans = element.querySelectorAll('span');
+  const wrapper = element.closest('.box');
+
+  gsap.set(spans, { x: 0, y: 0, rotate: 0, opacity: 1 });
+
+  ScrollTrigger.create({
+    trigger: wrapper,
+    start: "bottom 50%",
+    end: "top 60%",
+    toggleActions: "play reverse play reverse",
+    // markers: true,
+
+    onEnter: () => {
+      spans.forEach(span => {
+        gsap.to(span, {
+          x: gsap.utils.random(-200, 200),
+          y: gsap.utils.random(-150, -300),
+          rotate: gsap.utils.random(-360, 360),
+          opacity: 0,
+          duration: 1.2,
+          ease: "power4.out"
+        });
+      });
+    },
+
+    onLeaveBack: () => {
+      spans.forEach(span => {
+        gsap.to(span, {
+          x: 0,
+          y: 0,
+          rotate: 0,
+          opacity: 1,
+          duration: 1,
+          ease: "power2.inOut"
+        });
+      });
+    }
+  });
+}
