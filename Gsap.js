75cf87a8324b5d1ffd6c8c7fae8ae5d4470c8b66
Sugeng Saputra <141040198+HappyWeddingDays@users.noreply.github.com>
2025-06-10 17:06:19 +0700

    Update Gsap.js

--- Gsap.js
+++ Gsap.js
@@ -29,7 +29,7 @@ targets.forEach(el => {
   ScrollTrigger.create({
     trigger: wrapper,
     start: "bottom 80%",         // Efek dimulai saat pembungkus masuk 60% viewport
-    end: "top 20%",
+    end: "top 60%",
     markers: true,
     toggleActions: "play reverse play reverse",
     
